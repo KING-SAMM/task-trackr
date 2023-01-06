d6986f8c449c11596297a2774482398f7795a9f0
kcsamm <kcsamm11@gmail.com>
2023-01-06 11:31:14 +0100

    Created middleware router functions

---
+++ server/middleware/middlewrae.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"encoding/json"
+	"log"
+	"net/http"
+	"github.com/gorrila/mux"
+)
+
+func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func TaskComplete(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UndoTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteAllTasks(w http.ResponseWriter, r *http.Request) {
+
+}
